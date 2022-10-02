fd280af668547429029f4f0db92856c09ef83674
GFou <gfouchard20@outlook.com>
2022-10-02 18:18:41 -0400

    add date to workout model

--- models/workout.js
+++ models/workout.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const workoutSchema = new Schema({
   name: String,
   pump: Boolean,
+  date: Date,
   client: { type: Schema.Types.ObjectId, ref: "Profile" }
 }, {
   timestamps: true
