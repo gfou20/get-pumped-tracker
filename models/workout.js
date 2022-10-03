a0bc74c00f951b2686e5b9c4eb21cd189b1b4d18
GFou <gfouchard20@outlook.com>
2022-10-03 10:52:04 -0400

    embed setScehma in workout model

--- models/workout.js
+++ models/workout.js
@@ -2,10 +2,22 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const setSchema = new Schema({
+  sets: {
+    type: Number, min: 0, max: 5
+  },
+  reps: {
+    type: Number, min: 0, max: 100
+  }
+}, {
+  timestamps: true
+})
+
 const workoutSchema = new Schema({
   name: String,
   pump: Boolean,
   date: Date,
+  sets: [setSchema],
   client: { type: Schema.Types.ObjectId, ref: "Profile" }
 }, {
   timestamps: true
