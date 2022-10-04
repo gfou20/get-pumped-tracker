06aa87cf2c476a5a50a5a98f84876ef57ad61a2f
GFou <gfouchard20@outlook.com>
2022-10-04 11:29:35 -0400

    embed goalSchema in profiles

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,17 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const goalSchema = new Schema({
+  exerGoal: String,
+  time: Date,
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  goals: [goalSchema]
 }, {
   timestamps: true
 })
